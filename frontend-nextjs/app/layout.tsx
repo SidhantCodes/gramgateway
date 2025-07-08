187a6d81e5f5ea5959ea1d8ee31cea6555d80a6e
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-07-08 15:12:38 +0200

    dummy

--- frontend-nextjs/app/layout.tsx
+++ frontend-nextjs/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-app-gradient min-h-screen`}>
+      <body className={`${inter.className} bg-[#FFF0DD] min-h-screen`}>
         {children}
         <Toaster 
           position="top-right"
